6fcca278165725341719286f8a1bdb8ec83cc0c1
basileLeroy <basile2105@gmail.com>
2025-03-07 15:24:33 +0100

    Change plugin name to moodle standards

--- lang/en/mod_punchclock.php
+++ lang/en/mod_punchclock.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Strings for component 'punch_clock', language 'en'
+ * Strings for component 'punchclock', language 'en'
  *
- * @package    mod_punch_clock
+ * @package    mod_punchclock
  * @copyright  2025 onwards Basile Leroy {@link https://basileleroy.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['editstudentpack'] = 'Edit Student Pack Settings';
+$string['modulename'] = 'Punch Clock';
 $string[''] = '';
--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 /**
  * A Moodle plugin to keep track of attendance sessions in a flexible manner.
  *
- * @package    mod_punch_clock
+ * @package    mod_punchclock
  * @copyright  2025 onwards Basile Leroy {@link https://basileleroy.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -12,4 +12,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2025030700;
 $plugin->requires  = 2024100100;
-$plugin->component = 'mod_punch_clock';
+$plugin->component = 'mod_punchclock';
+$plugin->maturity  = MATURITY_ALPHA; // Or MATURITY_STABLE when ready.
+$plugin->release   = 'v1.0-alpha'; // Human-readable version.
