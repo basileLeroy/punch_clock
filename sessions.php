0f7044f9daf4c481fd96c5bd54b73da615e49709
basileLeroy <basile2105@gmail.com>
2025-03-31 11:40:45 +0200

    Update : Change page rendering to efficiently show filters and table

--- sessions.php
+++ sessions.php
@@ -11,10 +11,11 @@
 require_once("../../config.php");
 require_once($CFG->dirroot . '/mod/punchclock/classes/forms/filterform.php');
 require_once($CFG->dirroot . '/mod/punchclock/classes/enums/filtercontrols.php');
-require_once($CFG->dirroot . '/mod/punchclock/classes/output/page_filter_controls.php');
 
-use \mod_punchclock\output\page_filter_controls;
-use \mod_punchclock\enums\filter_controls;
+use mod_punchclock\forms\filterform;
+use mod_punchclock\enums\filter_controls;
+use mod_punchclock\output\sessions_overview;
+use mod_punchclock\output\time_range_selector;
 
 $id         = required_param('id', PARAM_INT);
 $view       = optional_param('view', filter_controls::WEEK, PARAM_INT);
@@ -30,9 +31,33 @@ $PAGE->set_title(get_string('modulename', 'mod_punchclock'));
 $PAGE->set_heading('Sessions');
 $PAGE->set_context($context);
 
-$filtercontrols = new page_filter_controls($id, $view);
+$filtercontrols = [];
+
+$buttons = new time_range_selector($id, $view);
+$mform = new filterform(null, ['id' => $id, 'view' => $view]);
+
+$filtercontrols = [
+    "calendar" => $mform->render(),
+    "buttons" => $buttons->get(),
+];
+
+$table = new sessions_overview([]);
+
+if ($mform->is_submitted() && $mform->is_validated()) {
+    $data = $mform->get_data();
+    if ($data) {
+        $date = $data->date ?? null;
+        // Redirect with the date parameter while keeping existing ones
+        redirect(new moodle_url('/mod/punchclock/sessions.php', [
+            'id' => $id,
+            'view' => $view,
+            'date' => $date
+        ]));
+    }
+}
 
 echo $OUTPUT->header();
 echo "<h3>Handle your sessions</h3>";
-echo $OUTPUT->render_from_template('mod_punchclock/components/filterbar', $filtercontrols->render());
+echo $OUTPUT->render_from_template('mod_punchclock/components/filterbar', $filtercontrols);
+echo $table->render();
 echo $OUTPUT->footer();
