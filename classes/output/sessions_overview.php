cf6bc7e9eb6486e411eafa0bcc887dd7688f3379
basileLeroy <basile2105@gmail.com>
2025-04-02 16:36:25 +0200

    Update : Change form layout to include action buttons and a checkbox column

--- classes/output/sessions_overview.php
+++ classes/output/sessions_overview.php
@@ -12,7 +12,7 @@ namespace mod_punchclock\output;
 
 defined('MOODLE_INTERNAL') || die();
 
-use mod_punchclock\enums\filter_controls;
+use mod_punchclock\output\table_actions;
 use moodle_url;
 
 /**
@@ -56,6 +56,7 @@ class sessions_overview
         $this->sessions = [];
         foreach ($sessions as $session) {
             $prepared = [
+                'unix_date' => $session->date,
                 'date' => userdate($session->date, '%b %e, %Y'),
                 'view_link' => new moodle_url('/mod/punchclock/view.php', [
                     'id' => $this->cmid,
@@ -75,69 +76,6 @@ class sessions_overview
     }
 
     /**
-     * Prepares navigation elements with navigation links
-     * @return string Navigation element.
-     */
-    private function insert_navigation()
-    {
-        global $PAGE;
-
-        // Get URL parameters, ensuring defaults
-        $id = required_param('id', PARAM_INT);
-        $view = optional_param('view', filter_controls::WEEK, PARAM_INT);
-        $date = optional_param('date', time(), PARAM_INT);
-
-        switch ($view) {
-            case filter_controls::MONTH:
-                $currentMonth = date('F Y', $date);
-                $prevMonth = strtotime('-1 month', $date);
-                $nextMonth = strtotime('+1 month', $date);
-
-                return '<div class="navigation text-end mb-3">'
-                    . '<div class="btn-group">'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $prevMonth]) . '" class="btn btn-secondary">◀️ Previous Month</a>'
-                    . '<span class="btn btn-light disabled">' . $currentMonth . '</span>'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $nextMonth]) . '" class="btn btn-secondary">Next Month ▶️</a>'
-                    . '</div>'
-                    . '</div>';
-
-            case filter_controls::WEEK:
-                $startOfWeek = strtotime('Monday this week', $date);
-                $endOfWeek = strtotime('Sunday this week', $date);
-                $prevWeek = strtotime('-1 week', $date);
-                $nextWeek = strtotime('+1 week', $date);
-
-                $dateRange = date('M j', $startOfWeek) . ' - ' . date('M j', $endOfWeek);
-
-                return '<div class="navigation text-end mb-3">'
-                    . '<div class="btn-group">'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $prevWeek]) . '" class="btn btn-secondary">◀️ Previous Week</a>'
-                    . '<span class="btn btn-light disabled">' . $dateRange . '</span>'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $nextWeek]) . '" class="btn btn-secondary">Next Week ▶️</a>'
-                    . '</div>'
-                    . '</div>';
-
-            case filter_controls::DAY:
-                $currentDate = date('M j, Y', $date);
-                $prevDay = strtotime('-1 day', $date);
-                $nextDay = strtotime('+1 day', $date);
-
-                return '<div class="navigation text-end mb-3">'
-                    . '<div class="btn-group">'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $prevDay]) . '" class="btn btn-secondary">◀️ Yesterday</a>'
-                    . '<span class="btn btn-light disabled">' . $currentDate . '</span>'
-                    . '<a href="' . new moodle_url($PAGE->url, ['id' => $id, 'view' => $view, 'date' => $nextDay]) . '" class="btn btn-secondary">Tomorrow ▶️</a>'
-                    . '</div>'
-                    . '</div>';
-
-            case filter_controls::ALL:
-            default:
-                return '';
-        }
-    }
-
-
-    /**
      * Renders the table as HTML
      *
      * @return string HTML output of the table
@@ -146,10 +84,13 @@ class sessions_overview
     {
         global $OUTPUT;
 
+        $tableactions = new table_actions();
+
         $html = '<table class="table table-striped table-hover">';
-        $html .= $this->insert_navigation();
+        $html .= $tableactions->render();
         $html .= '<thead class="text-center">';
         $html .= '<tr>';
+        $html .= '<th class="text-center"><input type="checkbox" /></th>';
         $html .= '<th style="width: 33%;" class="text-center">Date</th>';
         $html .= '<th class="text-center">View</th>';
         $html .= '<th class="text-center">Update</th>';
@@ -165,6 +106,7 @@ class sessions_overview
             // Render session rows
             foreach ($this->sessions as $row) {
                 $html .= '<tr>';
+                $html .= '<td class="text-center"><input name="dates[]" value="' . $row['unix_date'] . '" type="checkbox" /></td>';
                 $html .= '<td class="text-center" style="width: 33%;">' . htmlspecialchars($row['date']) . '</td>';
                 $html .= '<td class="text-center"><a href="' . htmlspecialchars($row['view_link']) . '" class="btn btn-link">See page</a></td>';
                 $html .= '<td class="text-center"><a href="' . htmlspecialchars($row['update_link']) . '" class="btn btn-sm btn-primary">Edit</a></td>';
