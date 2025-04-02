bb4b5b1a31b1054721cfed62405ddd3b1870b192
basileLeroy <basile2105@gmail.com>
2025-04-02 11:32:40 +0200

    Fix : Change code to read unix timestamps correctly and return array of unix timestamps

--- classes/utils/date_utils.php
+++ classes/utils/date_utils.php
@@ -13,41 +13,44 @@ defined('MOODLE_INTERNAL') || die();
 
 use mod_punchclock\enums\filter_controls;
 
-
 class date_utils {
     /**
      * Get date range for the selected view mode.
      *
      * @param int $view One of filter_controls::DAY/WEEK/MONTH/ALL.
      * @param string|null $date Reference date (Y-m-d). Default: today.
-     * @return array ['start' => 'Y-m-d', 'end' => 'Y-m-d'].
+     * @return array ['start' => int, 'end' => int] (Unix timestamps).
      */
     public static function get_date_range(int $view, ?string $date = null): array {
-        $timestamp = $date ? strtotime($date) : time();
+        $timestamp = $date ? strtotime(date('Y-m-d H:i:s', $date)) : time();
 
         switch ($view) {
             case filter_controls::DAY:
-                return [
-                    'start' => date('Y-m-d', $timestamp),
-                    'end' => date('Y-m-d', $timestamp),
-                ];
+                $start = strtotime("midnight", $timestamp);
+                $end = strtotime("tomorrow", $start) - 1;
+                break;
 
             case filter_controls::WEEK:
-                $start = date('Y-m-d', strtotime('monday this week', $timestamp));
-                $end = date('Y-m-d', strtotime('sunday this week', $timestamp));
-                return compact('start', 'end');
+                $dayOfWeek = date('N', $timestamp);
+                $start = strtotime("-" . ($dayOfWeek - 1) . " days", $timestamp);
+                $start = strtotime("midnight", $start);
+
+                $end = strtotime("+" . (7 - $dayOfWeek) . " days", $timestamp);
+                $end = strtotime("tomorrow", $end) - 1;
+                break;
 
             case filter_controls::MONTH:
-                $start = date('Y-m-01', $timestamp); // First day of month.
-                $end = date('Y-m-t', $timestamp);   // Last day of month.
-                return compact('start', 'end');
+                $start = strtotime(date('Y-m-01 00:00:00', $timestamp));
+                $end = strtotime(date('Y-m-t 23:59:59', $timestamp));
+                break;
 
             case filter_controls::ALL:
             default:
-                return [
-                    'start' => '1970-01-01', // Arbitrary early date.
-                    'end' => '2038-01-19',   // Arbitrary far future date.
-                ];
+                $start = 0;
+                $end = strtotime('+10 years') - 1;
+                break;
         }
-    }
+
+        return compact('start', 'end');
+    }    
 }
